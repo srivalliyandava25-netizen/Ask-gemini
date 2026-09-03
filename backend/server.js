2a5b202d7580583ed1b7ee4ee77c4568381c3c34
Srivalli <srivalli123@gmail.com>
2026-09-03 11:53:02 +0530

    cors has updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
 
@@ -7,7 +7,7 @@ const ai = require("./gemini");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../frontend")));
